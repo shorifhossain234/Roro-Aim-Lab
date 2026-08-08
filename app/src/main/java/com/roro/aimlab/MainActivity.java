77a74a9169a4cf175feb56f941a2728bb4d1d61f
shorifhossain234 <aminibovanya543@gmail.com>
2026-08-08 07:07:01 +0000

    Test: minimal + AdMob only

--- app/src/main/java/com/roro/aimlab/MainActivity.java
+++ app/src/main/java/com/roro/aimlab/MainActivity.java
@@ -2,15 +2,34 @@ package com.roro.aimlab;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        MobileAds.initialize(this, initializationStatus -> {});
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
         TextView tv = new TextView(this);
-        tv.setText("Hello Roro");
-        tv.setTextSize(30);
-        setContentView(tv);
+        tv.setText("Hello Roro + AdMob");
+        tv.setTextSize(24);
+        layout.addView(tv);
+
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3071340264379141/1648765767");
+        layout.addView(adView);
+        adView.loadAd(new AdRequest.Builder().build());
+
+        setContentView(layout);
     }
 }
